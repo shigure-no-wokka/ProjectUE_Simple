a39494aaa036a7f14f11555bd59620bddd38d5b1
shin <soutakushin@163.com>
2025-03-19 00:52:08 +0800

    #--story 资源管理器插件（测试版）
    提交内容：定义总表MainDataTable结构，定义测试用PicTable，实现总表随游戏加载

---
+++ Plugins/TestResourceManager/Source/TestResourceManager/Public/ResourceManager.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ResourceManager.generated.h"
+
+const FString MainDataTablePath = TEXT("/TestResourceManager/DataTables/MainDataTableConfig.MainDataTableConfig");
+
+/**
+ * 
+ */
+UCLASS()
+class TESTRESOURCEMANAGER_API UResourceManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override { return true; }
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+private:
+	UPROPERTY()
+	UDataTable* MainDataTable;
+};
+
+inline void UResourceManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	// TODO: 加载总DataTable表，并加载其中的固定加载表
+	MainDataTable = LoadObject<UDataTable>(this, *MainDataTablePath);
+    if (MainDataTable)
+    {
+        UE_LOG(LogTemp, Log, TEXT("UResourceManager, 成功加载插件 DataTable"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("UResourceManager, 加载插件 DataTable 失败！请检查路径是否正确"));
+    }
+}
+
+inline void UResourceManager::Deinitialize()
+{
+	Super::Deinitialize();
+
+	// TODO: 卸载总DataTable表
+}
---
+++ Plugins/TestResourceManager/Source/TestResourceManager/Public/TableDefine.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TableDefine.generated.h"
+
+// 总表结构（不要动这里）
+USTRUCT(BlueprintType)
+struct FMainDataTableRow : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Table")
+	FString TableName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Table")
+	FSoftObjectPath TablePath;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Table")
+	bool IsLoadedWithGame;
+	
+};
+
+// ----- 在这里定义新的结构体
+
+USTRUCT(BlueprintType)
+struct FTestPicTableRow : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "TestPicTable")
+	FString PicName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "TestPicTable")
+	TSoftObjectPtr<UTexture2D> PicPath;
+	
+};
+
