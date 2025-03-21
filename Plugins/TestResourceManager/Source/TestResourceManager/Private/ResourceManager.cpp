6d1fa48e808b2a19ef8a4c81f5e421f15fe3a278
shin <soutakushin@163.com>
2025-03-22 01:48:50 +0800

    #--story 资源管理器 提交内容：移动方法定义位置

--- Plugins/TestResourceManager/Source/TestResourceManager/Private/ResourceManager.cpp
+++ Plugins/TestResourceManager/Source/TestResourceManager/Private/ResourceManager.cpp
@@ -2,3 +2,26 @@
 
 
 #include "ResourceManager.h"
+
+void UResourceManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	// TODO: 加载总DataTable表，并加载其中的固定加载表
+	MainDataTable = LoadObject<UDataTable>(this, *MainDataTablePath);
+	if (MainDataTable)
+	{
+		UE_LOG(LogTemp, Log, TEXT("UResourceManager, 成功加载插件 DataTable"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("UResourceManager, 加载插件 DataTable 失败！请检查路径是否正确"));
+	}
+}
+
+void UResourceManager::Deinitialize()
+{
+	Super::Deinitialize();
+
+	// TODO: 卸载总DataTable表
+}
--- Plugins/TestResourceManager/Source/TestResourceManager/Public/ResourceManager.h
+++ Plugins/TestResourceManager/Source/TestResourceManager/Public/ResourceManager.h
@@ -25,26 +25,3 @@ private:
 	UPROPERTY()
 	UDataTable* MainDataTable;
 };
-
-inline void UResourceManager::Initialize(FSubsystemCollectionBase& Collection)
-{
-	Super::Initialize(Collection);
-
-	// TODO: 加载总DataTable表，并加载其中的固定加载表
-	MainDataTable = LoadObject<UDataTable>(this, *MainDataTablePath);
-    if (MainDataTable)
-    {
-        UE_LOG(LogTemp, Log, TEXT("UResourceManager, 成功加载插件 DataTable"));
-    }
-    else
-    {
-        UE_LOG(LogTemp, Error, TEXT("UResourceManager, 加载插件 DataTable 失败！请检查路径是否正确"));
-    }
-}
-
-inline void UResourceManager::Deinitialize()
-{
-	Super::Deinitialize();
-
-	// TODO: 卸载总DataTable表
-}
